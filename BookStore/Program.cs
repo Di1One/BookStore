b088bfd904432283b3b462fbdd789bee74351f64
Di1One <kirillwdovenko@mail.ru>
2024-06-26 14:57:43 +0300

    Configure DB connection

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -13,6 +13,11 @@ namespace BookStore
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddDbContext<BookStoreDbContext>(options =>
+            {
+                options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(BookStoreDbContext)));
+            });
+
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
